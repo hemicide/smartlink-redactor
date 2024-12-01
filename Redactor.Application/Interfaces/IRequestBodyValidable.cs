51e76016f0ec3280754896e7aec86c3baa4c8c2c
hristov <com.hemicide@gmail.com>
2024-12-01 15:38:06 +0300

    init branch

--- Redactor.Application/Interfaces/IRequestBodyValidable.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Redactor.Application.Interfaces
-{
-    public interface IRequestBodyValidable
-    {
-        public (bool ok, IDictionary<string, string[]> problems) Validate(IDictionary<string, object> args);
-    }
-}
